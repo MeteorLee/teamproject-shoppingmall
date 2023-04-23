0168f1f0c9cf000bba15d49ffdec51e9aaa33d21
gh934 <116714655+gh934@users.noreply.github.com>
2023-04-23 20:55:21 +0900

    Add files via upload

---
+++ src/main/java/project/finalproject1backend/FinalProject1BeckendApplication.java
@@ -0,0 +1,13 @@
+package project.finalproject1backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProject1BeckendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProject1BeckendApplication.class, args);
+    }
+
+}
