a13799125e439faa9d4bf805151834f7d6d41044
gh999 <gh934@naver.com>
2023-04-24 15:42:32 +0900

    자동배포 확인용 컨트롤러

---
+++ src/main/java/project/finalproject1backend/config/SecurityConfig.java
@@ -0,0 +1,115 @@
+package project.finalproject1backend.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Log4j2
+@RequiredArgsConstructor
+@EnableMethodSecurity
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+//    private final JwtTokenProvider jwtTokenProvider;
+//    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+//    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+//
+//    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration
+    ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    //    @Bean
+//    AuthenticationManager authenticationManager(AuthenticationManagerBuilder builder) throws Exception {
+//        return builder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder()).and().build();
+//    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.addExposedHeader("Authorization");
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        log.info("----------------configure-------------");
+        return http
+                .httpBasic().disable()
+                .formLogin().disable()
+                .cors()
+                .and()
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests()
+//                .requestMatchers("/user/**").authenticated()
+//                .requestMatchers("/manager/**").hasAnyRole("MANAGER", "ADMIN")
+//                .requestMatchers("/admin/**").hasRole("ADMIN")
+//                .anyRequest().permitAll()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+
+                .requestMatchers("/", "/signup", "/login","/now").permitAll()
+
+//                .requestMatchers("/schedule/admin/**", "/account/admin/**").hasRole("ADMIN")
+//                .requestMatchers(HttpMethod.POST, "/schedule/**", "/account/**").hasAnyRole("USER","ADMIN")
+                .anyRequest().authenticated()
+//                .and()
+//                .addFilterBefore(new JwtTokenFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+//
+//                .exceptionHandling()
+//                .authenticationEntryPoint(jwtAuthenticationEntryPoint) //  401 UNAUTHORIZED
+//                .accessDeniedHandler(jwtAccessDeniedHandler) // 403 FORBIDDEN
+                .and().build();
+    }
+
+    // 필터에서 제외시킬 url 등록
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        log.info("-------------web configure-------------");
+//        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+        return (web) -> web.ignoring().requestMatchers(
+                "/v3/api-docs/**",
+                "/swagger-ui/**"
+        );
+        // "/**"  임시로 모든 보안 해제시 셋팅!
+    }
+}
