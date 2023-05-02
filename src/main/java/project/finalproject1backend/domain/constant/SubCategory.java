2a7a7ae47dae89049444c696195da3eb4f65cfd1
yuseong00 <114868050+yuseong00@users.noreply.github.com>
2023-05-02 23:34:42 +0900

    feat: product 도메인 기본틀 작성

---
+++ src/main/java/project/finalproject1backend/domain/constant/SubCategory.java
@@ -0,0 +1,32 @@
+package project.finalproject1backend.domain.constant;
+
+import lombok.Getter;
+import lombok.Setter;
+import project.finalproject1backend.domain.AuditingFields;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="sub_category")
+@Getter
+@Setter
+public class SubCategory extends AuditingFields {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "main_category", nullable = false, length = 20)
+    private MainCategory mainCategory;
+
+    @OneToMany(mappedBy = "subCategory")
+    private Set<DetailCategory> detailCategories = new HashSet<>();
+
+
+}
