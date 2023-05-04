b2f8d64e596a92b41ddec7b44bf4539c2e60a3f9
gh999 <gh934@naver.com>
2023-05-04 17:05:35 +0900

    feat: cl- Issue #16

---
+++ src/main/java/project/finalproject1backend/config/ControllerAdvice.java
@@ -0,0 +1,34 @@
+package project.finalproject1backend.config;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import project.finalproject1backend.dto.ErrorDTO;
+
+import java.nio.file.AccessDeniedException;
+
+@org.springframework.web.bind.annotation.ControllerAdvice
+public class ControllerAdvice {
+
+    @ExceptionHandler(IllegalAccessException.class)
+    public String handleIllegalAccessException(IllegalAccessException e) {
+        return "redirect:/login";
+    }
+
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException e) {
+        return new ResponseEntity<>(new ErrorDTO("400",e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<?> handleAccessDeniedException(AccessDeniedException e){
+        return new ResponseEntity<>(new ErrorDTO("403",e.getMessage()), HttpStatus.FORBIDDEN);
+    }
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<?> handleBadCredentialsException(BadCredentialsException e){
+        return new ResponseEntity<>(new ErrorDTO("401",e.getMessage()), HttpStatus.UNAUTHORIZED);
+    }
+}
