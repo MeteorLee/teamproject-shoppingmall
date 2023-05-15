b4306b87329b482ffb922d7ce5efa7ae05f27e70
황의진 <hwanguijin7@gmail.com>
2023-05-15 21:44:58 +0900

    문의 인증 오류 발생(해결 못함)

---
+++ src/main/java/project/finalproject1backend/controller/Inquiry/BuyInquiryController.java
@@ -0,0 +1,50 @@
+package project.finalproject1backend.controller.Inquiry;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import project.finalproject1backend.domain.AttachmentFile;
+import project.finalproject1backend.dto.ErrorDTO;
+import project.finalproject1backend.dto.PrincipalDTO;
+import project.finalproject1backend.dto.ResponseDTO;
+import project.finalproject1backend.dto.inquiry.BuyInquiryDTO;
+import project.finalproject1backend.service.Inquiry.BuyInquiryService;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/")
+@RestController
+public class BuyInquiryController {
+
+    private final BuyInquiryService buyInquiryService;
+
+    @Tag(name = "API 문의하기", description = "판매 문의하기 api 입니다.")
+    @Operation(summary = "문의생성 메서드", description = "문의생성 메서드입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation", content = @Content(schema = @Schema(implementation = ResponseDTO.class))),
+            @ApiResponse(responseCode = "400", description = "bad request operation", content = @Content(schema = @Schema(implementation = ErrorDTO.class)))
+    })
+    @PostMapping(value = "/account/saleInquiry/register",consumes = {MediaType.MULTIPART_FORM_DATA_VALUE,MediaType.APPLICATION_JSON_VALUE}, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> buyInquiryCreat (@Parameter(hidden = true) @AuthenticationPrincipal PrincipalDTO principal, @RequestPart(value = "requestDTO") @Valid BuyInquiryDTO requestDTO,
+                                              BindingResult bindingResult, @RequestPart(required = false) List<MultipartFile> buyImageList) {
+
+        return buyInquiryService.buyInquiryCreat(requestDTO, buyImageList, principal);
+
+    }
+}
---
+++ src/main/java/project/finalproject1backend/service/Inquiry/BuyInquiryService.java
@@ -0,0 +1,86 @@
+package project.finalproject1backend.service.Inquiry;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+import project.finalproject1backend.domain.AttachmentFile;
+import project.finalproject1backend.domain.Inquiry.BuyInquiry;
+import project.finalproject1backend.domain.User;
+import project.finalproject1backend.dto.PrincipalDTO;
+import project.finalproject1backend.dto.ResponseDTO;
+import project.finalproject1backend.dto.UploadDTO;
+import project.finalproject1backend.dto.inquiry.BuyInquiryDTO;
+import project.finalproject1backend.jwt.JwtTokenProvider;
+import project.finalproject1backend.repository.AttachmentFileRepository;
+import project.finalproject1backend.repository.Inquiry.BuyInquiryRepository;
+import project.finalproject1backend.repository.UserRepository;
+import project.finalproject1backend.util.UploadUtil;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BuyInquiryService {
+
+    private final BuyInquiryRepository buyInquiryRepository;
+
+    private  final UserRepository userRepository;
+
+    private final AttachmentFileRepository attachmentFileRepository;
+
+    private final UploadUtil uploadUtil;
+
+        private String path = "C:\\Users\\user\\Downloads\\my";  //로컬 테스트용
+
+//    private String path = "/home/ubuntu/FinalProject/upload/inquiry";  // 배포용
+
+    public ResponseEntity<?> buyInquiryCreat(BuyInquiryDTO requestDTO, List<MultipartFile> buyImageList, PrincipalDTO principal) {
+
+        Optional<User> user = userRepository.findByUserId(principal.getUserId());
+
+        BuyInquiry buyInquiry = BuyInquiry.builder()
+                .buyInquiryId(user.get())
+                .category(requestDTO.getCategory())
+                .product(requestDTO.getProduct())
+                .amount(requestDTO.getAmount())
+                .content(requestDTO.getContent())
+                .estimateWishDate(requestDTO.getEstimateWishDate())
+                .deliveryWishDate(requestDTO.getDeliveryWishDate())
+                .build();
+
+        buyInquiryRepository.save(buyInquiry);
+
+        for (MultipartFile m: buyImageList) {
+            UploadDTO uploadDTO = uploadUtil.upload(m,path);
+
+            attachmentFileRepository.save(AttachmentFile.builder()
+                    .fileName(uploadDTO.getFileName())
+                    .filePath(path)
+                    .originalFileName(uploadDTO.getOriginalName())
+                    .thumbFileName(uploadDTO.getThumbFileName())
+                    .buyImage(buyInquiry)
+                    .build());
+
+        }
+
+        return new ResponseEntity<>(new ResponseDTO("200","success"), HttpStatus.OK);
+    }
+
+//    public ResponseEntity<?> BuyInquiry(BuyInquiryDTO buyInquiryDTO) {
+//
+//        if(buyInquiryRepository.existsByUserId(buyInquiryDTO.getUserId())) {
+//            throw new IllegalArgumentException("existId");
+////            return new ResponseEntity<>(new ErrorDTO("400","existId"), HttpStatus.BAD_REQUEST);
+//        }
+//        Optional<User> user = buyInquiryRepository.findByUserId(buyInquiryDTO.getUserId());
+//
+//        return new ResponseEntity<>(new ResponseDTO("200","success"), HttpStatus.OK);
+//    }
+}
